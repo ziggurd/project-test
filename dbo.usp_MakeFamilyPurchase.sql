1111cbcd1ac999d4ab3b72611a0d61777e9c5757
Yan Kadyrov <yankadyrov@yandex.ru>
2023-09-12 11:34:36 +0300

    second change

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -3,19 +3,20 @@
 	@FamilySurname varchar(255)
 AS
 Begin
-
-
-					--Проверка сущетсвования семьи в таблице Family
-If NOT Exists (Select * From family Where Surname=@FamilySurName)
-Begin
-	Throw 50000, 'Такой семьи не существует', 1;
-End;
-
-
+--Проверка сущетсвования семьи в таблице Family
+If NOT Exists 
+	(Select * From family 
+		Where Surname=@FamilySurName)
+	Begin
+		Throw 50000, 'Такой семьи не существует', 1;
+	End;
 Update family
-set BudgetValue = (Select SUM(Value) from Basket Where ID_Family =(
-						Select ID_Family from Family Where Surname =@FamilySurname))
-					Where Surname=@FamilySurName;
+	set BudgetValue = (
+		Select SUM(Value) from Basket 
+		Where ID_Family =(
+			Select ID_Family from Family 
+			Where Surname =@FamilySurname))
+	Where Surname=@FamilySurName;
 --
 End;
 
