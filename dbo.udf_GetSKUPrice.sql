a9adf27e1865bb1779488374ae3f266d9214179a
Yan Kadyrov <yankadyrov@yandex.ru>
2023-09-12 10:20:46 +0300

    create functions

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,11 @@
+﻿--Создание функции
+Create Function dbo.udf_GetSKUPrice ( @ID_SKU INT)
+Returns decimal (18,2)
+As
+Begin
+	Declare @Price Decimal (18,2)
+	Select @Price=SUM(Value)/SUM(Quantity)
+	From dbo.Basket
+	Where ID_SKU=@ID_SKU
+	Return @Price
+End
